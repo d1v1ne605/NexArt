6e8df72b393af198c27e567ecc5ad4e3ea99865a
dungnv18 <nvandung296@gamil.com>
2025-10-16 14:15:22 +0700

    done setup subgraph

---
+++ server/scripts/call-inc.ts
@@ -0,0 +1,21 @@
+import { network } from "hardhat";
+const { ethers } = await network.connect("ganache");
+
+async function main() {
+  const CONTRACT_ADDRESS = "0x8aB215541656867Fd7c2689060E17E8A73bb44D2";
+  const counter = await ethers.getContractAt("Counter", CONTRACT_ADDRESS);
+
+  console.log(`Current value of x: ${await counter.x()}`);
+
+  console.log("Calling incBy(10)...");
+  const tx = await counter.incBy(5);
+  await tx.wait();
+
+  console.log("Transaction confirmed!");
+  console.log(`New value of x: ${await counter.x()}`);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
